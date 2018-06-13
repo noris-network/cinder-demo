5a1b9e6ef45df346138553f51879c76f5ded8ede
Oz Tiram <oz.tiram@noris.de>
2018-06-13 14:07:57 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -112,6 +112,22 @@ todo_include_todos = False
 # a list of builtin themes.
 html_theme = 'alabaster'
 
+html_sidebars = {
+    '**': [
+        'about.html',
+        'navigation.html',
+    ]
+}
+
+# Theme options are theme-specific and customize the look and feel of a
+# theme further.  For a list of options available for each theme, see the
+# documentation.
+#
+
+html_theme_options = {
+    'fixed_sidebar': True,
+}
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
